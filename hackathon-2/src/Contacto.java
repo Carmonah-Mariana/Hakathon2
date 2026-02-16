5c46e36da40566df25edf663601e40f3a8155db8
Teo <teo.charles@hotmail.com>
2026-02-16 09:49:39 -0700

    Archivo inicial

---
+++ hackathon-2/src/Contacto.java
@@ -0,0 +1,20 @@
+class Contacto {
+
+    private String nombre;
+    private int numero;
+
+    public Contacto(String nombre, int numero){
+        this.setNombre(nombre);
+        this.setNumero(numero);
+    }
+
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public int getNumero() {return numero;}
+    public void setNumero(int numero) {this.numero = numero;}
+
+
+
+
+}
---
+++ hackathon-2/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Crear agenda //
+        // Crear agenda //
+
+        // Agregar Contacto //
+        // Agregar Contacto //
+
+        // Existe Contacto //
+        // Existe Contacto //
+
+        // Listar Contactos //
+        // Listar Contactos //
+
+        // Buscar Contacto //
+        // Buscar Contacto //
+
+        // Eliminar Contacto //
+        // Eliminar Contacto //
+
+        // Agenda llena //
+        // Agenda llena //
+
+        // Espacios libres //
+        // Espacios libres //
+
+        // funcion Main //
+        // funcion Main //
+
+
+    }
+}
